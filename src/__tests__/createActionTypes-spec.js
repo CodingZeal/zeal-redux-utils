70b5ae56bf5999be61e146d9735be54604856d9c
Luke Barbuto <luke.barbuto@gmail.com>
2016-12-01 07:50:48 -0800

    Add test for createActionTypes

---
+++ src/__tests__/createActionTypes-spec.js
@@ -0,0 +1,16 @@
+import createActionTypes from '../createActionTypes'
+
+describe('createActionTypes', () => {
+  test('creates a map with namespeced values', () => {
+    const namespace = 'module'
+    const actionList = ['FOO', 'BAR', 'BAZ']
+
+    const result = createActionTypes(namespace, actionList)
+
+    expect(result).toEqual({
+      FOO: '~module/FOO',
+      BAR: '~module/BAR',
+      BAZ: '~module/BAZ'
+    })
+  })
+})
