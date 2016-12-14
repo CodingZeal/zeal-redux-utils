77eb864913a9a555cba8a3cd205bc308ff805350
Randy Coulman <rcoulman@gmail.com>
2016-12-14 08:07:08 -0800

    Allow globalized selectors to be curried

--- src/__tests__/globalizeSelectors-spec.js
+++ src/__tests__/globalizeSelectors-spec.js
@@ -1,4 +1,4 @@
-import { prop } from 'ramda'
+import { curry, prop } from 'ramda'
 
 import globalizeSelectors from '../globalizeSelectors'
 
@@ -8,25 +8,34 @@ describe('globalizeSelectors', () => {
       numbers: [2, 3, 4]
     }
   }
-
   const localState = prop('section')
-
-  const selectors = {
-    numbers: prop('numbers'),
-    numberAt: (index, state) => state.numbers[index]
-  }
-
-  const globalized = globalizeSelectors(localState, selectors)
+  const numbers = prop('numbers')
+  const numberAt = (index, state) => state.numbers[index]
+  const globalized = globalizeSelectors(localState, {
+    numbers,
+    numberAt,
+    curriedNumberAt: curry(numberAt)
+  })
 
   context('with a single-argument selector', () => {
-    test('allows the selector to work from the global state', () => {
+    it('allows the selector to work from the global state', () => {
       expect(globalized.numbers(globalState)).toEqual([2, 3, 4])
     })
   })
 
   context('with a multi-argument selector', () => {
-    test('allows the selector to work from the global state', () => {
-      expect(globalized.numberAt(1, globalState)).toEqual(3)
+    it('allows the selector to work from the global state', () => {
+      expect(globalized.numberAt(1, globalState)).toBe(3)
+    })
+  })
+
+  context('with a curried selector', () => {
+    it('allows the selector to work from the global state', () => {
+      expect(globalized.curriedNumberAt(2, globalState)).toBe(4)
+    })
+
+    it('can be called in a curried fashion', () => {
+      expect(globalized.curriedNumberAt(2)(globalState)).toBe(4)
     })
   })
 })
